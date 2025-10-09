c8780e0df9b6882f1a4626b34c9bca46a0c25e33
Dimitry Deveza <dimydeveza@gmail.com>
2025-10-09 20:33:47 -0300

    rev_04

---
+++ 01_revisao/rev_04/main.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int n;
+  scanf("%d", &n);
+  printf("%o", n);
+
+  exit(EXIT_SUCCESS);
+}
+
+// KKKKKKKKKKKKKKKKKKKKKKKKKKKKK vou deixar assim KKKKKKKKKKKKKKKKKKKKKKK
