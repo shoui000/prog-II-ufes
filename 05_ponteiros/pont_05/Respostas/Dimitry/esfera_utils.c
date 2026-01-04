aca15b383226ec3ee5b995fa536f78c47602207c
Dimitry Deveza <dimydeveza@gmail.com>
2026-01-03 23:35:48 -0300

    pont_05

---
+++ 05_ponteiros/pont_05/Respostas/Dimitry/esfera_utils.c
@@ -0,0 +1,21 @@
+#include "esfera_utils.h"
+
+// #define PI 3.14
+
+/**
+ * @brief Calcula o volume de uma esfera com raio R.
+ * @param R O raio da esfera.
+ * @param volume Um ponteiro para uma variável float para armazenar o volume calculado.
+ */
+void CalculaVolume (float R, float *volume) {
+  *volume = (4.0 / 3.0) * PI * R * R * R;
+}
+
+/**
+ * @brief Calcula a área de uma esfera com raio R.
+ * @param R O raio da esfera.
+ * @param area Um ponteiro para uma variável float para armazenar a área calculada.
+ */
+void CalculaArea (float R, float *area) {
+  *area = 4 * PI * R * R;
+}
